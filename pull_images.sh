9b5629dac1df3d01fc8711f238b70b09da09eefa
Gustav <gustav@davcon.se>
2024-06-07 14:33:37 +0200

    fixing

--- pull_images.sh
+++ pull_images.sh
@@ -3,3 +3,4 @@ TOKEN=$(curl --silent --header  "Metadata-Flavor: Google" http://metadata.google
 ACCESS=$(echo ${TOKEN} | grep --extended-regexp --only-matching "(ya29.[0-9a-zA-Z._-]*)")
 /usr/bin/docker login -u oauth2accesstoken -p "${ACCESS}" https://europe-west1-docker.pkg.dev
 /usr/bin/docker pull europe-west1-docker.pkg.dev/reportall/cbuild/nginx:tag1
+/usr/bin/docker run --rm -v  /var/run/docker.sock:/var/run/docker.sock -v "/home/composer/.docker:/root/.docker" -v "/home/composer:/home/composer" -w="/home/composer" docker/compose:1.24.0 up -d --build
