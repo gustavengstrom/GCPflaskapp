ff562f03db3e801be019bce4cf0f3aa17150a62d
Gustav <gustav@davcon.se>
2024-06-07 17:13:43 +0200

    fix

--- pull_images.sh
+++ pull_images.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 TOKEN=$(curl --silent --header  "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token)
 ACCESS=$(echo ${TOKEN} | grep --extended-regexp --only-matching "(ya29.[0-9a-zA-Z._-]*)")
 /usr/bin/docker login -u oauth2accesstoken -p "${ACCESS}" https://europe-west1-docker.pkg.dev
