aaa92545ff4d3f912559e270fd1c366bc881587e
Gustav <gustav@davcon.se>
2024-06-08 06:12:02 +0200

    fix

--- pull_images.sh
+++ pull_images.sh
@@ -5,3 +5,4 @@ ACCESS=$(echo ${TOKEN} | grep --extended-regexp --only-matching "(ya29.[0-9a-zA-
 /usr/bin/docker login -u oauth2accesstoken -p "${ACCESS}" https://europe-west1-docker.pkg.dev
 /usr/bin/docker pull europe-west1-docker.pkg.dev/reportall/cbuild/nginx:tag1
 /usr/bin/docker run --rm -v  /var/run/docker.sock:/var/run/docker.sock -v "/home/composer/.docker:/root/.docker" -v "/home/composer:/home/composer" -w="/home/composer" docker/compose:1.24.0 up -d --build
+
